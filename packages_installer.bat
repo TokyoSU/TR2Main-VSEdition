7f3614b231bfcf08e0b3e73a3aa3109806bf0416
TokyoSU <77746747+TokyoSU@users.noreply.github.com>
2024-04-23 15:29:47 +0200

    Update packages_installer.bat

--- packages_installer.bat
+++ packages_installer.bat
@@ -7,7 +7,7 @@ vcpkg install dxsdk-d3dx:x86-windows
 vcpkg install spdlog:x86-windows
 vcpkg install rapidjson:x86-windows
 echo ========================================
-echo Installing X86 Windows Library
+echo Installing X64 Windows Library
 echo ========================================
 vcpkg install mimalloc[core,override,secure]:x64-windows
 vcpkg install dxsdk-d3dx:x64-windows
